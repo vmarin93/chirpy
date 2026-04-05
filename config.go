c7aeb0b3ec30a9c0759c796687b7f4d575364c45
Vasile Marin <vasilemarin93@gmail.com>
2026-04-05 22:09:18 +0100

    Add request counter middleware and metrics/reset endpoints
    Add apiConfig struct with atomic.Int32 hit counter
    
    Add middlewareMetricsInc to count requests to /app/
    
    Add /metrics endpoint to report hit count as plain text
    
    Add /reset endpoint to reset hit counter to zero

---
+++ config.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (conf *apiConfig) middlewareMetricsCount(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		conf.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (conf *apiConfig) getMetricsCount(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(fmt.Appendf([]byte{}, "Hits: %v", conf.fileServerHits.Load()))
+}
+
+func (conf *apiConfig) resetMetricsCount(w http.ResponseWriter, _ *http.Request) {
+	conf.fileServerHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
+	conf := &apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", conf.middlewareMetricsCount(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerHealth)
+	mux.HandleFunc("/metrics", conf.getMetricsCount)
+	mux.HandleFunc("/reset", conf.resetMetricsCount)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -19,8 +22,8 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
-func handlerHealth(res http.ResponseWriter, _ *http.Request) {
-	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	res.WriteHeader(http.StatusOK)
-	res.Write([]byte(http.StatusText(http.StatusOK)))
+func handlerHealth(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
