03251b31282ed92f4e280e3ad853c9319cabb2dd
Vasile Marin <vasilemarin93@gmail.com>
2026-04-05 19:11:53 +0100

    serve files from root
    configure server to listen to port 8080
    
    define multiplexer and register handler for root path at project root

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	const filepathRoot = "."
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
