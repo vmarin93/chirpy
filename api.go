33d3fe373c09723b6870968cf00697520a321890
Vasile Marin <vasilemarin93@gmail.com>
2026-04-07 19:42:54 +0100

    Add censorship validation to chirps
    wrote sanitze function to replace censored words in chirps
    
    wrote table tests for the sanitization func
    
    tests cover 4 cases so far, nothing to sanitize, censor all caps, censor partly caps and censor all

--- api.go
+++ api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func handlerHealth(w http.ResponseWriter, _ *http.Request) {
@@ -14,11 +15,16 @@ func handlerHealth(w http.ResponseWriter, _ *http.Request) {
 
 func handlerChirpValidation(w http.ResponseWriter, r *http.Request) {
 	const maxChirpLen = 140
+	censored := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
 	type parameters struct {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		Sanitized string `json:"cleaned_body"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
@@ -28,9 +34,19 @@ func handlerChirpValidation(w http.ResponseWriter, r *http.Request) {
 	if len(params.Body) > maxChirpLen {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 	} else {
-		respondWithJson(w, http.StatusOK, returnVals{Valid: true})
+		respondWithJson(w, http.StatusOK, returnVals{
+			Sanitized: sanitize(params.Body, censored)})
 	}
+}
 
+func sanitize(s string, censored map[string]struct{}) string {
+	words := strings.Split(s, " ")
+	for i, word := range words {
+		if _, ok := censored[strings.ToLower(word)]; ok {
+			words[i] = "****"
+		}
+	}
+	return strings.Join(words, " ")
 }
 
 func respondWithJson(w http.ResponseWriter, code int, payload any) {
